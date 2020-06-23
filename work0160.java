1d3b4ae8f25f827dbe692f51cc6adf011fc5e44d
smallsmartlc <1346683197@qq.com>
2020-06-23 16:06:01 +0800

    2020-6-23--0160--相交链表

--- work0160.java
+++ work0160.java
@@ -2,7 +2,32 @@ package Leetcode.smart;
 
 public class work0160 {
 	public static void main(String[] args) {
-		
+		work0160 w = new work0160();
+		ListNode l1 = new ListNode(4,1);
+		ListNode l2 = new ListNode(5,0,1);
+		ListNode l3 = new ListNode(8,4,5);
+		ListNode cur = l1;
+		while(cur.next!=null) {
+			cur = cur.next;
+		}
+		cur.next = l3;
+		cur = l2;
+		while(cur.next!=null) {
+			cur = cur.next;
+		}
+		cur.next = l3;
+		System.out.println(l1);
+		System.out.println(l2);
+		System.out.println(w.getIntersectionNode(l1, l2));
 	}
+	public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode cur1 = headA;
+        ListNode cur2 = headB;
+        while(cur1!=cur2) {
+        	cur1 = cur1==null?headB:cur1.next;
+        	cur2 = cur2==null?headA:cur2.next;
+        }
+        return cur1;
+    }
 }
 
