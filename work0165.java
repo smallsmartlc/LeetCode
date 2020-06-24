dbdf2dccab040783b6d26e82f17d83f06ed684f6
smallsmartlc <1346683197@qq.com>
2020-06-24 23:01:36 +0800

    2020-6-24--0165--比较版本号

---
+++ work0165.java
@@ -0,0 +1,27 @@
+package Leetcode.smart;
+
+public class work0165 {
+	public static void main(String[] args) {
+		work0165 w = new work0165();
+		System.out.println(w.compareVersion("1.01", "1.001"));
+	}
+	public int compareVersion(String version1, String version2) {
+		String[] v1 = version1.split("\\.");
+		String[] v2 = version2.split("\\.");
+		for (int i = 0; i < Math.min(v1.length, v2.length); i++) {
+			if(Integer.parseInt(v1[i])<Integer.parseInt(v2[i])) return -1;
+			else if(Integer.parseInt(v1[i])>Integer.parseInt(v2[i])) return 1;
+		}
+		if(v1.length<v2.length) {
+			for (int i = v1.length; i < v2.length; i++) {
+				if(Integer.parseInt(v2[i])>0) return -1;
+			}
+		}else if(v1.length>v2.length) {
+			for (int i = v2.length; i < v1.length; i++) {
+				if(Integer.parseInt(v1[i])>0) return 1;
+			}
+		}
+		return 0;
+    }
+}
+
