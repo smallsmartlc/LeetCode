5b9775370ea029b0081595a868232ddbc784f21a
smallsmartlc <1346683197@qq.com>
2020-06-27 11:48:56 +0800

    2020-6-27--0169--多数元素

---
+++ work0169.java
@@ -0,0 +1,32 @@
+package Leetcode.smart;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class work0169 {
+	public static void main(String[] args) {
+		work0169 w = new work0169();
+		int[] nums = {3,2,3};
+		System.out.println(w.majorityElement(nums));
+	}
+    public int majorityElement0(int[] nums) {
+    	//¹þÏ£±í·¨
+    	Map<Integer, Integer> map = new HashMap<>();
+    	for (int i : nums) {
+			if(!map.containsKey(i)) {
+				map.put(i, 1);
+			}else {
+				map.put(i, map.get(i)+1);
+			}
+			if(map.get(i)>nums.length/2) return i;
+		}
+    	return -1;
+    }
+    public int majorityElement(int[] nums) {
+    	//ÅÅÐò·¨
+    	Arrays.sort(nums);
+    	return nums[nums.length/2];
+    }
+}
+
