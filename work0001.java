35b78cd06d46cf2f583a2d7342b866a701d32ab7
smallsmartlc <1346683197@qq.com>
2020-07-01 08:29:09 +0800

    2020-7-1--0001--两数之和

--- work0001.java
+++ work0001.java
@@ -1,5 +1,6 @@
 package Leetcode.smart;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,4 +18,19 @@ public class work0001 {
         }
         throw new IllegalArgumentException("No two sum solution");
     }
+	
+	public int[] twoSum2(int[] nums, int target) {
+		Map<Integer, Integer> map = new HashMap<>();
+		for (int i = 0; i < nums.length; i++) {
+			int com = target-nums[i];
+			if(map.containsKey(com)) return new int[]{map.get(com),i};
+			map.put(nums[i], i);
+		}
+		return new int[] {};
+	}
+	public static void main(String[] args) {
+		work0001 w = new work0001();
+		int[] nums = {2,7,11,15};
+		System.out.println(Arrays.toString(w.twoSum2(nums, 9)));
+	}
 }
