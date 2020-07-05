61d8b58719bdb11fc4239810d5ae921df563ecfd
smallsmartlc <1346683197@qq.com>
2020-07-05 09:02:18 +0800

    2020-7-5--0009--回文数

--- work0009.java
+++ work0009.java
@@ -5,12 +5,11 @@ public class work0009 {
 		if(x2<0) return false;
 		int x = x2;
 		int res = 0;
-		while(true) {
+		while(x>0) {
 			res = res*10 + x%10;
 			x=x/10;
-			if(x<=0) break;
 		}
-		return res==x2? true:false;
+		return res==x2;
 	}
 
 	public static void main(String[] args) {
