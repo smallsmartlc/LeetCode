0c28090e4e1c522226cfa1b0bc8b1d003419641d
smallsmartlc <1346683197@qq.com>
2020-07-06 13:20:58 +0800

    2020-7-6--0014--最长公共前缀

--- work0014.java
+++ work0014.java
@@ -4,10 +4,10 @@ public class work0014 {
 	public String longestCommonPrefix(String[] strs) {
 		if	(strs.length==0) return "";
 		String res = "";
-		for (int i = 0; i<strs[0].length(); i++) {
+		first:for (int i = 0; i<strs[0].length(); i++) {
 			char temp = strs[0].charAt(i);
 			for (int j = 0; j < strs.length; j++) {
-				if(i>=strs[j].length()) return res.replaceAll(" ", "");
+				if(i>=strs[j].length()) break first;//使用return,这里练习break跳出多层循环
 				if (temp!= strs[j].charAt(i)) {
 					return res.replaceAll(" ", "");
 				}
