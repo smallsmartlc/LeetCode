87bc002597929eb9450c64c807ffc8601d6272f0
smallsmartlc <1346683197@qq.com>
2020-07-06 11:36:54 +0800

    2020-7-6--0011--盛最多水的容器

--- work0011.java
+++ work0011.java
@@ -1,21 +1,22 @@
 package Leetcode.smart;
 
 public class work0011 {
-	public int maxArea(int[] height) {
-		int l = 0;
-		int r = height.length-1;
-		int res = 0;
-		while(l<=r) {
-			if(Math.min(height[l],height[r])*(r-l)>res) res= Math.min(height[l],height[r])*(r-l);
-			if(height[l]<height[r]) l++;
-			else r--;
-		}
-		
-		return res;
-    }
 	public static void main(String[] args) {
 		work0011 w = new work0011();
 		int[] height = new int[] {1,8,6,2,5,4,8,3,7};
 		System.out.println(w.maxArea(height));
 	}
+	public int maxArea(int[] height) {
+		//Ë«Ö¸Õë
+		int l = 0;
+		int r = height.length-1;
+		int res = 0;
+		while(l<r) {
+			int area = Math.min(height[l], height[r])*(r-l);
+			if(area > res) res = area;
+			if(height[l]<height[r]) l++;
+			else r--;
+		}
+		return res;
+    }
 }
