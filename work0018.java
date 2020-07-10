53b698c7f940ccdaf28666479e861402ebef6783
smallsmartlc <1346683197@qq.com>
2020-07-10 17:25:37 +0800

    2020-7-10--0018--四数之和

--- work0018.java
+++ work0018.java
@@ -10,55 +10,46 @@ public class work0018 {
 		List<List<Integer>> res = new ArrayList<>();
 		if (len < 4) return res;
 		Arrays.sort(nums);
-		if (target < nums[0] + nums[1] + nums[2] + nums[3] || 
-			target > nums[len-1] + nums[len-2] + nums[len-3] + nums[len-4]) {
-	            return res;
-	    }
+		if(target<nums[0] + nums[1] + nums[2] + nums[3]||target > nums[len-1] + nums[len-2] + nums[len-3] + nums[len-4]) {
+			return res;
+		}
 		for(int i = 0;i<len-3;i++) {
-			if(i>0&&nums[i]==nums[i-1]) continue;
+			if(i>0&&nums[i] == nums[i-1]) continue;
 			if (target < nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3]) {
 				break;
 			}
-			if (target > nums[i] + nums[len - 1] + nums[len - 2] + nums[len - 3]) {
-                continue;
-            }
-			for (int j = i + 1; j < len - 2; j++) {
-				if(j>i+1&&nums[j]==nums[j-1]) continue;
-				if (target < nums[i] + nums[j] + nums[j+1] + nums[j+2]) {
+			if(target>nums[i]+nums[len-1]+nums[len-2]+nums[len-3]) {
+				continue;
+			}
+			for (int j = i+1; j < len-2; j++) {
+				if(j>i+1&&nums[j] == nums[j-1]) continue;
+				if (target < nums[i] + nums[j] + nums[j + 1] + nums[j + 2]) {
 					break;
 				}
-				if (target > nums[i] + nums[j] + nums[len-1] + nums[len-2]) {
-	                continue;
-	            }
-				int L = j + 1, R = len - 1;
-				while(L<R) {
-					int sum = nums[i] + nums[j] + nums[L] + nums[R];
+				if(target>nums[i] + nums[j] + nums[len-1] + nums[len - 2]) {
+					continue;
+				}
+				int l = j+1;int r = len-1;
+				while(l<r) {
+					int sum = nums[i]+nums[j]+nums[l]+nums[r];
 					if(sum==target) {
-						res.add(Arrays.asList(nums[i],nums[j],nums[L],nums[R]));
-						L++;
-                        while (L < R && nums[L - 1] == nums[L]) {
-                            L++;
-                        }
-                        R--;
-                        while (L < R && nums[R + 1] == nums[R]) {
-                            R--;
-                        }
-					}else if(sum>=target) {
-						R--;
-                        while (L < R && nums[R + 1] == nums[R]) {
-                            R--;
-                        }
+						res.add(Arrays.asList(nums[i],nums[j],nums[l],nums[r]));
+						l++;
+						while(l<r&&nums[l-1]==nums[l]) l++;
+						r--;
+						while(l<r&&nums[r+1]==nums[r]) r--;
+					}else if(sum<target) {
+						l++;
+						while(l<r&&nums[l-1]==nums[l]) l++;
 					}else {
-						L++;
-                        while (L < R && nums[L - 1] == nums[L]) {
-                            L++;
-                        }
+						r--;
+						while(l<r&&nums[r+1]==nums[r]) r--;
 					}
 				}
 			}
 		}
 		return res;
-    }
+	}
 	public static void main(String[] args) {
 		work0018 w = new work0018();
 		int[] nums = new int[]{1, 0, -1, 0, -2, 2};
