e238350b21249e5a8c69be31987db41dea967e5f
smallsmartlc <1346683197@qq.com>
2020-07-14 11:41:34 +0800

    2020-7-14--0028--实现strStr

--- work0028.java
+++ work0028.java
@@ -1,21 +1,17 @@
 package Leetcode.smart;
 
 public class work0028 {
-	public int strStr(String haystack, String needle) {
-		if (needle.equals("")) {
-			return 0;
-		}
-		int len = needle.length();
-		for(int i = 0;i<haystack.length()-len+1;i++) {
-			if(haystack.substring(i, i+len).equals(needle)) {
-				return i;
-			}
-		}
-		return -1;
-    }
 	public static void main(String[] args) {
 		work0028 w = new work0028();
-		int[] nums = { 1, 1, 2, 2, 2, 2, 3, 4, 5 };
-		System.out.println(w.strStr("a", "a"));
+		System.out.println(w.strStr2("a", "a"));
+	}
+	public int strStr2(String haystack, String needle) {
+		int length = haystack.length();
+		int len = needle.length();
+		if(len==0) return 0;
+		for (int start = 0; start+len <= length; start++) {
+			if(haystack.substring(start, start+len).equals(needle)) return start;
+		}
+		return -1;
 	}
 }
