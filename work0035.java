fb5cd0e02990437d4758b5a69749c9f6fea395db
smallsmartlc <1346683197@qq.com>
2020-07-17 13:24:38 +0800

    2020-7-17--0035--搜索插入位置

--- work0035.java
+++ work0035.java
@@ -1,7 +1,5 @@
 package Leetcode.smart;
 
-import java.util.Stack;
-
 public class work0035 {
 	public int searchInsert(int[] nums, int target) {
 		for(int i =0 ;i<nums.length;i++) {
@@ -12,18 +10,17 @@ public class work0035 {
 	public int binarySearchInsert(int[] nums, int target) {
 		int left = 0;
 		int right = nums.length-1;
-		while(left<=right) {
-			int mid = (left+right)/2;
-			if(nums[mid]==target) {
-				return mid;
-			}else if(nums[mid]<=target){
-				left = mid +1;
-			}else{
-				right = mid -1;
+		while(left <= right) {
+			int mid = (right + left)/2;
+			if(nums[mid] == target) {return mid;}
+			if(nums[mid]< target) {
+				left = mid + 1;
+			}else {
+				right = mid - 1;
 			}
 		}
 		return left;
-    }
+	}
 	public static void main(String[] args) {
 		work0035 w = new work0035();
 		int[] nums = new int[]{1,3,5,6};
