adc52e66c4685f7764c20d8ee334949f69e82453
smallsmartlc <1346683197@qq.com>
2020-07-17 13:52:14 +0800

    2020-7-17--0033--搜索旋转排序数组

--- work0033.java
+++ work0033.java
@@ -11,13 +11,13 @@ public class work0033 {
 			if(nums[mid]==target) return mid;
 			if(nums[left]<=nums[mid]) {
 				if(nums[left]<=target&&nums[mid]>=target) {
-					right = mid -1 ;
+					right = mid - 1 ;
 				}else {
-					left = mid +1;
+					left = mid + 1;
 				}
 			}else {
 				if(nums[right]>=target&&nums[mid]<=target) {
-					left = mid +1 ;
+					left = mid + 1 ;
 				}else {
 					right = mid -1;
 				}
