ca2d91f9793442665d54598302226ff9d1fafce1
smallsmartlc <1346683197@qq.com>
2020-07-20 10:08:45 +0800

    2020-7-20--0039--组合总和

--- work0039.java
+++ work0039.java
@@ -5,28 +5,30 @@ import java.util.List;
 
 public class work0039 {
 	List<List<Integer>> res = new ArrayList<>();
+
 	public List<List<Integer>> combinationSum(int[] candidates, int target) {
 		if (candidates == null || candidates.length == 0 || target < 0) {
-            return res;
-        }
+			return res;
+		}
 		List<Integer> list = new ArrayList<>();
-        process(0,candidates, target, list);
+		process(0, candidates, target, list);
 		return res;
-    }	
-	public void process(int start,int[] candidates,int target,List<Integer> list){
-		if(target<0) return;
-		if(target==0) res.add(new ArrayList<>(list));
+	}
+
+	public void process(int start, int[] candidates, int target, List<Integer> list) {
+		if (target < 0) return;
+		if (target == 0) res.add(new ArrayList<>(list));
 		else {
-			for(int i = start;i<candidates.length;i++) {
+			for (int i = start; i < candidates.length; i++) {
 				list.add(candidates[i]);
-				process(i, candidates, target-candidates[i], list);
-				list.remove(list.size()-1);
+				process(i, candidates, target - candidates[i], list);
+				list.remove(list.size() - 1);
 			}
 		}
 	}
 	public static void main(String[] args) {
 		work0039 w = new work0039();
-		int[] nums = new int[] {2,3,6,7};
+		int[] nums = new int[] { 2, 3, 6, 7 };
 		System.out.println(w.combinationSum(nums, 7));
 	}
 }
