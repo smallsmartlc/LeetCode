5fb05ad258579913df9e8e74936959514a8e82ec
smallsmartlc <1346683197@qq.com>
2020-07-22 10:26:35 +0800

    2020-7-22--0053--最大子序和

--- work0053.java
+++ work0053.java
@@ -1,9 +1,5 @@
 package Leetcode.smart;
 
-
-/**
-
- */
 public class work0053 {
 	public int maxSubArray(int[] nums) {
 		int res = nums[0];
@@ -18,10 +14,23 @@ public class work0053 {
 		}
 		return res;
     }
+	public int maxSubArray2(int[] nums) {
+		//尝试使用动态规划
+		int len = nums.length;
+		int[] dp = new int[len];
+		dp[0] = nums[0];
+		for (int i = 1; i < dp.length; i++) {
+			dp[i] = dp[i-1]>0?dp[i-1]+nums[i]:nums[i];
+		}
+		int res = dp[0];
+		for (int i : dp) {
+			if(res<i) res = i;
+		}
+		return res;
+	}
 	public static void main(String[] args) {
 		work0053 w = new work0053();
 		int[] nums = new int[] {-1,2,3};
 		System.out.println(w.maxSubArray(nums));
-		
 	}
 }
