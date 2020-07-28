e0e2dc4455ad95e8fbe4e7f040808e5bf21a71ad
smallsmartlc <1346683197@qq.com>
2020-07-28 13:51:49 +0800

    2020-7-28--0063--不同路径II

--- work0063.java
+++ work0063.java
@@ -1,45 +1,26 @@
 package Leetcode.smart;
 
-
-/**
-	回溯法以及动态规划
- */
 public class work0063 {
 	public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-		int m = obstacleGrid[0].length; 
 		int n = obstacleGrid.length;
-		if(m==0||n==0) return 0;
-		if(obstacleGrid[n-1][m-1]==1||obstacleGrid[0][0]==1) return 0;
-		int[][] temp = new int[n][m];
-		temp[0][0] = 1;
-		for (int i = 1; i < m; i++) {
-			//上边界
-			if(obstacleGrid[0][i]==1||temp[0][i-1] == 0) {
-				temp[0][i] = 0;
-			}else {
-				temp[0][i] = 1;
-			} 
+		int m = obstacleGrid[0].length;
+		int[][] dp = new int[n][m];
+		for (int i = 0; i < m; i++) {
+			if(obstacleGrid[0][i] !=0 ) break;
+			dp[0][i] = 1;
 		}
-		for (int i = 1; i < n; i++) {
-			//左边界
-			if(obstacleGrid[i][0]==1||temp[i-1][0] == 0) {
-				temp[i][0] = 0;
-			}else {
-				temp[i][0] = temp[i-1][0];
-			} 
+		for (int i = 0; i < n; i++) {
+			if(obstacleGrid[i][0] !=0 ) break;
+			dp[i][0] = 1;
 		}
 		for (int i = 1; i < n; i++) {
 			for (int j = 1; j < m; j++) {
-				if(obstacleGrid[i][j]==1) {
-					temp[i][j] = 0;
-				}else {
-					temp[i][j] = temp[i-1][j]+temp[i][j-1];
-				}
+				if(obstacleGrid[i][j] != 0 ) continue;
+				dp[i][j] = dp[i-1][j] + dp[i][j-1];
 			}
 		}
-		LecoUtil.LecoPrint(temp);
-		return temp[n-1][m-1];
-	}
+		return dp[n-1][m-1];
+    }
 	public int res = 0;
 	public int uniquePathsWithObstacles2(int[][] obstacleGrid) {
 		//递归,超时
@@ -73,7 +54,6 @@ public class work0063 {
 			{0,1,0},
 			{0,0,0}
 		};
-		int[][] nums2 = new int[][] {{1,0}};
 		LecoUtil.LecoPrint(nums); 
 		System.out.println(w.uniquePathsWithObstacles(nums));
 	}
