e9c29ed7c0ea697926bb4bf088347d1fa3274f06
smallsmartlc <1346683197@qq.com>
2020-07-29 13:39:41 +0800

    2020-7-29--0070--爬楼梯

--- work0070.java
+++ work0070.java
@@ -4,15 +4,13 @@ public class work0070 {
 	public int climbStairs(int n) {
 		//动态规划:第i阶只能由第i-1阶跳一步或第i-2阶跳两步
 		//res[i] = res[i-1]+res[i-2];
-		if(n==0) return 0;
-		if(n==1) return 1;
-		int res[] = new int[n];
-		res[0]=1; 
-		res[1]=2;
+		int dp[] = new int[n];
+		dp[0]=1; 
+		if(n > 1) dp[1]=2;
 		for (int i = 2; i < n; i++) {
-			res[i] = res[i-1]+res[i-2];
+			dp[i] = dp[i-1]+dp[i-2];
 		}
-		return res[n-1];
+		return dp[n-1];
 	}
 	public int climbStairs2(int n) {
 		//数学法,斐波那契数列
@@ -24,6 +22,12 @@ public class work0070 {
 			now = res-now;
 		}
 		return res;
+	}
+	public int climbStairs3(int n) {
+		//面向测试编程
+		int[] a = new int[]{1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597,2584,4181,6765,10946,17711,28657,46368,75025,121393,196418,317811,514229,832040,1346269,2178309,3524578,5702887,9227465,14930352,24157817,39088169,63245986,102334155,165580141,267914296,433494437,701408733,1134903170,1836311903};
+		return a[n-1];
+		//时间复杂度o(1),狗头
 	}
 	public static void main(String[] args) {
 		work0070 w= new work0070();
