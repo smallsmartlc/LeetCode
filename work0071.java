2a550d7893197f2c6a02dd6338fbc7b19a9a4995
smallsmartlc <1346683197@qq.com>
2020-07-30 15:49:59 +0800

    2020-7-30--0071--简化路径

--- work0071.java
+++ work0071.java
@@ -4,26 +4,25 @@ import java.util.Stack;
 
 public class work0071 {
 	public String simplifyPath(String path) {
-        String[] paths = path.split("/");
-        Stack<String> stack = new Stack<>();
-        for (String str : paths) {
-        	if(str.equals(""))continue;
+		String[] paths = path.split("/");
+		Stack<String> stack = new Stack<>();
+		for (String str : paths) {
+			if(str.equals("")) continue;
 			if(str.equals(".")) continue;
 			if(str.equals("..")) {
-				if(!stack.empty()) {
-					stack.pop();
-				}
+				if(!stack.empty()) stack.pop();
 				continue;
 			}
 			stack.push(str);
 		}
-        if(stack.empty()) return "/";
-        StringBuilder res = new StringBuilder();
-        while (!stack.empty()) {
-        	res.insert(0, "/"+stack.pop());
+		if(stack.empty()) return "/";
+		StringBuilder res = new StringBuilder();
+		while (!stack.empty()) {
+			res.insert(0, stack.pop());
+			res.insert(0, "/");
 		}
-        return res.toString();
-    }
+		return res.toString();
+	}
 	public static void main(String[] args) {
 		work0071 w= new work0071();
 		System.out.println(w.simplifyPath("/a//b////c/d//././/.."));
