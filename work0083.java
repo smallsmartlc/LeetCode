691342c97cf2d49d650244b4538c6114a8bdb9c0
smallsmartlc <1346683197@qq.com>
2020-08-13 13:30:32 +0800

    2020-8-13--0082--删除排序链表中的重复元素 II

--- work0083.java
+++ work0083.java
@@ -3,12 +3,12 @@ package Leetcode.smart;
 
 public class work0083 {
 	public ListNode deleteDuplicates(ListNode head) {
-		if(head==null||head.next==null) return head;
+		if (head == null || head.next == null) return head;
 		ListNode realHead = new ListNode(0);
 		realHead.next = head;
 		ListNode now = realHead.next;
-		while(now!=null&&now.next!=null) {
-			while(now.next!=null&&now.val==now.next.val) {
+		while (now != null && now.next != null) {
+			while (now.next != null && now.val == now.next.val) {
 				now.next = now.next.next;
 			}
 			now = now.next;
