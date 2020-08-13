c0857f8c1a1d6a9239ba47c9c7be9dfe60fa1099
smallsmartlc <1346683197@qq.com>
2020-08-13 16:20:43 +0800

    2020-8-13--0086--分隔链表

--- work0086.java
+++ work0086.java
@@ -3,24 +3,23 @@ package Leetcode.smart;
 
 public class work0086 {
 	public ListNode partition(ListNode head, int x) {
-		if(head==null||head.next==null) return head;
-		ListNode realHead = new ListNode(0);
-		realHead.next = head;
-		ListNode small = realHead;
-		ListNode now = realHead;
-		while(now.next!=null) {
+		ListNode real = new ListNode();
+		real.next = head;
+		ListNode small = real;
+		ListNode now = real;
+		while (now.next != null) {
 			ListNode pre = now;
 			now = now.next;
-			if(now.val<x) {
+			if (now.val < x) {
 				ListNode temp = now;
-				pre.next = now.next;
+				pre.next = temp.next;
 				temp.next = small.next;
 				small.next = temp;
 				small = temp;
 			}
 		}
-		return realHead.next;
-    }
+		return real.next;
+	}
 	public static void main(String[] args) {
 		work0086 w= new work0086();
 		ListNode head = new ListNode(1,4,3,2,5,2);
