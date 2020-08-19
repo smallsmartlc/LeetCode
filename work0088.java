c71c77207c1e7b57f7ae36bd43b829d3a00a1f59
smallsmartlc <1346683197@qq.com>
2020-08-19 13:18:42 +0800

    2020-8-19--0088--合并两个有序数组

--- work0088.java
+++ work0088.java
@@ -3,28 +3,25 @@ package Leetcode.smart;
 
 public class work0088 {
 	public void merge(int[] nums1, int m, int[] nums2, int n) {
-		if(nums2.length<1) return;
-		if(m==0) {
+		if (nums2.length < 1) return;
+		if (m == 0) {
 			System.arraycopy(nums2, 0, nums1, 0, n);
-			return;
-		} 
+		}
 		int[] n1 = new int[m];
 		System.arraycopy(nums1, 0, n1, 0, m);
-		int p1 = 0;
-		int p2 = 0;
-		int i = 0;
-		while (i<m+n&&p1<=m&&p2<=n) {
-			if(p1==m&&p2==n) break;
-			if(p2==n||(p1!=m&&n1[p1]<nums2[p2])) {
-				nums1[i] = n1[p1];
-				p1++;
-			}else{
-				nums1[i] = nums2[p2];
-				p2++;
+		int pn = 0;
+		int pm = 0;
+		while(pn<=n&&pm<=m) {
+			if(pn==n&&pm==m) break;
+			if(pn==n||(pm<m&&n1[pm]<nums2[pn])) {
+				nums1[pn+pm] = n1[pm];
+				pm++;
+			}else {
+				nums1[pn+pm] = nums2[pn];
+				pn++;
 			}
-			i++;
 		}
-    }
+	}
 	public static void main(String[] args) {
 		work0088 w= new work0088();
 		int[] nums1 = new int[] {1,0,0,0,0,0};
