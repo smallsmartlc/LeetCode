05f627425012f6d7fa836951e4645169bdf75e5e
smallsmartlc <1346683197@qq.com>
2020-09-04 14:32:32 +0800

    2020-9-4--0102--二叉树的层序遍历

--- work0102.java
+++ work0102.java
@@ -6,32 +6,35 @@ import java.util.List;
 import java.util.Queue;
 
 public class work0102 {
-	List<List<Integer>> res = new ArrayList<>();
-	public List<List<Integer>> levelOrder(TreeNode root) {
-		Queue<TreeNode> q = new LinkedList<>();
-		if (root!=null) {
-			q.add(root);
-		}
-		while(!q.isEmpty()) {
-			int n = q.size();
-			List<Integer> list = new LinkedList<>();
-			for (int i = 0; i < n; i++) {
-				TreeNode cur = q.poll();
-				list.add(cur.val);	
-				if(cur.left!=null) {
-					q.add(cur.left);
-				}
-				if(cur.right!=null) {
-					q.add(cur.right);
-				}
-			}
-			res.add(list);
-		}
-		return res;
+    List<List<Integer>> res = new ArrayList<>();
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        if (root != null) {
+            q.add(root);
+        }
+        while (!q.isEmpty()) {
+            List<Integer> list = new ArrayList<>();
+            int n = q.size();
+            for (int i = 0; i < n; i++) {
+                TreeNode poll = q.poll();
+                list.add(poll.val);
+                if (poll.left != null) {
+                    q.add(poll.left);
+                }
+                if (poll.right != null) {
+                    q.add(poll.right);
+                }
+            }
+            res.add(list);
+        }
+        return res;
     }
-	public static void main(String[] args) {
-		work0102 w= new work0102();
-		TreeNode p = TreeNode.treeNode(1,2,null,3,4,null,null,4,3);
-		System.out.println(w.levelOrder(p));
-	}
+
+
+    public static void main(String[] args) {
+        work0102 w = new work0102();
+        TreeNode p = TreeNode.treeNode(1, 2, null, 3, 4, null, null, 4, 3);
+        System.out.println(w.levelOrder(p));
+    }
 }
