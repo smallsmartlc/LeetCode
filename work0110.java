0440614a2fe40cdab1ad627d18583866dd63fa54
smallsmartlc <1346683197@qq.com>
2020-09-04 17:04:29 +0800

    2020-9-4--0110--平衡二叉树

--- work0110.java
+++ work0110.java
@@ -8,13 +8,9 @@ public class work0110 {
 		System.out.println(w.isBalanced(dif));
 	}
 	public boolean isBalanced(TreeNode root) {
-		if(root==null) return true;
-		int left = dif(root.left);
-		int right = dif(root.right);
-		return  Math.abs(left-right)<=1&&isBalanced(root.left)&&isBalanced(root.right);
-    }
-	private int dif(TreeNode root) {
-		if(root==null) return 0 ;
-		return Math.max(dif(root.left), dif(root.right))+1;
+		return root == null || (Math.abs(dfs(root.left) - dfs(root.right)) < 2 && isBalanced(root.left) && isBalanced(root.right));
+	}
+	private int dfs(TreeNode node) {
+		return node == null ? 0 : Math.max(dfs(node.left), dfs(node.right)) + 1;
 	}
 }
