63dadf9f24884f331690fd01c1d86f628c718c74
smallsmartlc <1346683197@qq.com>
2020-09-04 13:53:01 +0800

    2020-9-4--0100--相同的树

--- work0100.java
+++ work0100.java
@@ -2,19 +2,19 @@ package Leetcode.smart;
 
 
 public class work0100 {
-	public boolean isSameTree(TreeNode p, TreeNode q) {
-		if(p==null&&q==null) return true;
-		if(p==null) return false;
-		if(q==null) return false;
-		if(p.val==q.val) {
-			if(isSameTree(p.left, q.left)&&isSameTree(p.right, q.right)) return true;
-		}
-		return false;
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        if (p.val == q.val) {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+        return false;
     }
-	public static void main(String[] args) {
-		work0100 w= new work0100();
-		TreeNode p = TreeNode.treeNode(10,5,15);
-		TreeNode q = TreeNode.treeNode(10,5,null,null,15);
-		System.out.println(w.isSameTree(p, q));
-	}
+
+    public static void main(String[] args) {
+        work0100 w = new work0100();
+        TreeNode p = TreeNode.treeNode(10, 5, 15);
+        TreeNode q = TreeNode.treeNode(10, 5, null, null, 15);
+        System.out.println(w.isSameTree(p, q));
+    }
 }
