98f7def8b6a74ba8fe41c24c56291e2580727510
smallsmartlc <1346683197@qq.com>
2020-09-04 14:45:05 +0800

    2020-9-4--0103--二叉树的锯齿形层次遍历

--- work0103.java
+++ work0103.java
@@ -7,67 +7,36 @@ import java.util.List;
 import java.util.Queue;
 
 public class work0103 {
-	List<List<Integer>> res = new ArrayList<>();
-	public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-		Queue<TreeNode> q = new LinkedList<>();
-		if (root!=null) {
-			q.add(root);
-		}
-		boolean is_order_left = true;
-		while(!q.isEmpty()) {
-			int n = q.size();
-			LinkedList<Integer> list = new LinkedList<>();
-			for (int i = 0; i < n; i++) {
-				TreeNode cur = q.poll();
-				if (is_order_left) {
-					list.add(cur.val);	
-				}else {
-					list.addFirst(cur.val);	
-				}
-				if(cur.left!=null) {
-					q.add(cur.left);
-				}
-				if(cur.right!=null) {
-					q.add(cur.right);
-				}
-			}
-			res.add(list);
-			is_order_left = !is_order_left;
-		}
-		return res;
+    List<List<Integer>> res = new ArrayList<>();
+
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        if (root != null) {
+            q.add(root);
+        }
+        Boolean flag = true;
+        while (!q.isEmpty()) {
+            LinkedList<Integer> list = new LinkedList<>();
+            int n = q.size();
+            for (int i = 0; i < n; i++) {
+                TreeNode poll = q.poll();
+                if (flag) {
+                    list.add(poll.val);
+                } else {
+                    list.addFirst(poll.val);
+                }
+                if (poll.left != null) q.add(poll.left);
+                if (poll.right != null) q.add(poll.right);
+            }
+            res.add(list);
+            flag = !flag;
+        }
+        return res;
     }
-	public List<List<Integer>> zigzagLevelOrder2(TreeNode root) {
-		Queue<TreeNode> q = new LinkedList<>();
-		if (root!=null) {
-			q.add(root);
-		}
-		boolean is_order_left = true;
-		while(!q.isEmpty()) {
-			int n = q.size();
-			List<Integer> list = new LinkedList<>();
-			for (int i = 0; i < n; i++) {
-				TreeNode cur = q.poll();
-				list.add(cur.val);	
-				if(cur.left!=null) {
-					q.add(cur.left);
-				}
-				if(cur.right!=null) {
-					q.add(cur.right);
-				}
-			}
-			if(is_order_left) {
-				res.add(list);
-			}else {
-				Collections.reverse(list);
-				res.add(list);
-			}
-			is_order_left = !is_order_left;
-		}
-		return res;
+
+    public static void main(String[] args) {
+        work0103 w = new work0103();
+        TreeNode p = TreeNode.treeNode(3, 9, 20, null, null, 15, 7);
+        System.out.println(w.zigzagLevelOrder(p));
     }
-	public static void main(String[] args) {
-		work0103 w= new work0103();
-		TreeNode p = TreeNode.treeNode(3,9,20,null,null,15,7);
-		System.out.println(w.zigzagLevelOrder(p));
-	}
 }
