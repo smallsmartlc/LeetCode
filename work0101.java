c3c31c409bc0f703c7f25c48e6ab3a0b43b15829
smallsmartlc <1346683197@qq.com>
2020-09-04 14:00:18 +0800

    2020-9-4--0101--对称二叉树

--- work0101.java
+++ work0101.java
@@ -2,17 +2,23 @@ package Leetcode.smart;
 
 
 public class work0101 {
-	public boolean isSymmetric(TreeNode root) {
-        return isMirror(root, root);
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+        return isMirror(root.left, root.right);
     }
-	public boolean isMirror(TreeNode left,TreeNode right) {
-		if(left==null&&right==null) return true;
-		if(left==null||right==null) return false;
-		return left.val==right.val && isMirror(left.left, right.right) &&isMirror(left.right, right.left);
-	}
-	public static void main(String[] args) {
-		work0101 w= new work0101();
-		TreeNode p = TreeNode.treeNode(1,2,2,3,4,4,3);
-		System.out.println(w.isSymmetric(p));
-	}
+
+    private boolean isMirror(TreeNode left, TreeNode right) {
+        if (left == null && right == null) return true;
+        if (left == null || right == null) return false;
+        if (left.val == right.val) {
+            return isMirror(left.right, right.left) && isMirror(left.left, right.right);
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        work0101 w = new work0101();
+        TreeNode p = TreeNode.treeNode(1, 2, 2, 3, 4, 4, 3);
+        System.out.println(w.isSymmetric(p));
+    }
 }
