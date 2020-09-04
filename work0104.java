1bef07f2300bf7cbdf6da6cce2d7b7ca11080b87
smallsmartlc <1346683197@qq.com>
2020-09-04 15:12:19 +0800

    2020-9-4--0104--二叉树的最大深度

--- work0104.java
+++ work0104.java
@@ -4,12 +4,8 @@ import java.util.Stack;
 
 public class work0104 {
 	public int maxDepth(TreeNode root) {
-		if(root==null) return 0 ;
-		int depth = 0;
-		if(root.left!=null) depth = maxDepth(root.left)+1;
-		if(root.right!=null) depth = Math.max(depth, maxDepth(root.right))+1;
-		return depth;
-    }
+		return root==null?0:Math.max(root.left==null?0:maxDepth(root.left),root.right==null?0:maxDepth(root.right))+1;
+	}
 	public static void main(String[] args) {
 		work0104 w= new work0104();
 		TreeNode p = TreeNode.treeNode(0);
