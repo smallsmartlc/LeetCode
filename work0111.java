5c63601edb72212c70b7cd6f2971ef358eea840c
smallsmartlc <1346683197@qq.com>
2020-09-04 17:36:25 +0800

    2020-9-4--0112--路径总和

--- work0111.java
+++ work0111.java
@@ -2,19 +2,18 @@ package Leetcode.smart;
 
 
 public class work0111 {
-	public int minDepth(TreeNode root) {
-		if (root == null) return 0;
-		if ((root.left == null) && (root.right == null)) {
-			return 1;
-		}
-		int min = Integer.MAX_VALUE;
-		if(root.left!=null) min = Integer.min(min,minDepth(root.left));
-		if(root.right!=null) min= Integer.min(min,minDepth(root.right));
-		return min+1;
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) return false;
+        sum -= root.val;
+        if (root.left == null && root.right == null) {
+            return sum == 0;
+        }
+        return hasPathSum(root.left, sum) || hasPathSum(root.right, sum);
     }
-	public static void main(String[] args) {
-		work0111 w= new work0111();
-		TreeNode p = TreeNode.treeNode(1,2);
-		System.out.println(w.minDepth(p));
-	}
+
+    public static void main(String[] args) {
+        work0111 w = new work0111();
+        TreeNode p = TreeNode.treeNode(5, 4, 8, 11, null, 13, 4, 7, 2, null, null, null, 1);
+        System.out.println(w.hasPathSum(p, 22));
+    }
 }
