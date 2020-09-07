7f7872348ccd9180b00db2fa9fed0d262e1b2d07
smallsmartlc <1346683197@qq.com>
2020-09-07 22:27:15 +0800

    2020-9-7--0146--LRU缓存

--- work0146.java
+++ work0146.java
@@ -12,22 +12,25 @@ public class work0146 {
 		 System.out.println(param_1);
 	}
 }
-class LRUCache extends LinkedHashMap<Integer, Integer>{
+
+class LRUCache extends LinkedHashMap<Integer, Integer> {
 	private int capacity;
-    public LRUCache(int capacity) {
+
+	public LRUCache(int capacity) {
 		super(capacity, 0.75F, true);
-	    this.capacity = capacity;
-    }
-    
-    public int get(int key) {
-    	return super.getOrDefault(key, -1);
-    }
-    
-    public void put(int key, int value) {
-    	super.put(key, value);
-    }
-    @Override
-    protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
-        return size() > capacity; 
-    }
+		this.capacity = capacity;
+	}
+
+	public int get(int key) {
+		return super.getOrDefault(key, -1);
+	}
+
+	public void put(int key, int value) {
+		super.put(key, value);
+	}
+
+	@Override
+	protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+		return size() > capacity;
+	}
 }
