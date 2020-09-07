5a63865d30a183832a45053b2a65e67e33f3ff35
smallsmartlc <1346683197@qq.com>
2020-09-07 15:18:11 +0800

    2020-9-7--0129--求根到叶子节点数字之和

--- work0129.java
+++ work0129.java
@@ -4,16 +4,17 @@ package Leetcode.smart;
 public class work0129 {
 	public int sumNumbers(TreeNode root) {
         return dif(root, 0);
-    } 
-	public int dif(TreeNode root,int i) {
-		if (root==null) {
+    }
+
+	public int dif(TreeNode root, int i) {
+		if (root == null) {
 			return 0;
 		}
-		int temp = i*10 + root.val;
-		if(root.left==null&&root.right==null) {
+		int temp = i * 10 + root.val;
+		if (root.left == null && root.right == null) {
 			return temp;
 		}
-		return dif(root.left, temp)+dif(root.right, temp);
+		return dif(root.left, temp) + dif(root.right, temp);
 	}
 	public static void main(String[] args) {
 		work0129 w= new work0129();
