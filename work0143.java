294c369cb222750ef333e99550650b9ee2a84bb2
smallsmartlc <1346683197@qq.com>
2020-09-07 22:17:00 +0800

    2020-9-7--0143--重排链表

--- work0143.java
+++ work0143.java
@@ -1,12 +1,13 @@
 package Leetcode.smart;
 
 public class work0143 {
-	public void reorderList(ListNode head) {
-        if(head==null||head.next==null) return;
-        ListNode cur = head.next;ListNode pre = head;
-        while(cur.next!=null) {
-        	pre = cur;
-        	cur = cur.next;
+    public void reorderList(ListNode head) {
+        if (head == null || head.next == null) return;
+        ListNode cur = head.next;
+        ListNode pre = head;
+        while (cur.next != null) {
+            pre = cur;
+            cur = cur.next;
         }
         pre.next = null;
         ListNode temp = head.next;
@@ -14,11 +15,12 @@ public class work0143 {
         cur.next = temp;
         reorderList(head.next.next);
     }
-	public static void main(String[] args) {
-		work0143 w = new work0143();
- 		ListNode head = new ListNode(1,2,3,4,5);
- 		System.out.println(head);
- 		w.reorderList(head);
- 		System.out.println(head);
-	}
+
+    public static void main(String[] args) {
+        work0143 w = new work0143();
+        ListNode head = new ListNode(1, 2, 3, 4, 5);
+        System.out.println(head);
+        w.reorderList(head);
+        System.out.println(head);
+    }
 }
