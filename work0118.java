7c86430733ac87284095b0ffd75b648d4fcff037
smallsmartlc <1346683197@qq.com>
2020-09-07 13:51:47 +0800

    2020-9-7--0118--杨辉三角

--- work0118.java
+++ work0118.java
@@ -4,26 +4,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class work0118 {
-	public List<List<Integer>> generate(int numRows) {
-		List<List<Integer>> res = new ArrayList<>();
-		if(numRows==0) return res;
-		res.add(new ArrayList<>());
-		res.get(0).add(1);
-		if(numRows==1) return res;
-		for (int i = 1; i < numRows; i++) {
-			List<Integer> thisrow  = new ArrayList<>();
-			List<Integer> prerow  = res.get(i-1);
-			thisrow.add(1);
-			for (int j = 1; j < i; j++) {
-				thisrow.add(prerow.get(j-1)+prerow.get(j));
-			}
-			thisrow.add(1);
-			res.add(thisrow);
-		}
-		return res;
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (numRows == 0) return res;
+        res.add(new ArrayList<>());
+        res.get(0).add(1);
+        if (numRows == 1) return res;
+        for (int i = 1; i < numRows; i++) {
+            List<Integer> thisrow = new ArrayList<>();
+            List<Integer> prerow = res.get(i - 1);
+            thisrow.add(1);
+            for (int j = 1; j < i; j++) {
+                thisrow.add(prerow.get(j - 1) + prerow.get(j));
+            }
+            thisrow.add(1);
+            res.add(thisrow);
+        }
+        return res;
     }
-	public static void main(String[] args) {
-		work0118 w= new work0118();
-		System.out.println(w.generate(2));
-	}
+
+    public static void main(String[] args) {
+        work0118 w = new work0118();
+        System.out.println(w.generate(2));
+    }
 }
