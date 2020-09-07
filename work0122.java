c19e02da9f33740c0c2294768d028911cd97cbc9
smallsmartlc <1346683197@qq.com>
2020-09-07 14:58:33 +0800

    2020-9-7--0122--买卖股票的最佳时期 II

--- work0122.java
+++ work0122.java
@@ -1,27 +1,28 @@
 package Leetcode.smart;
 
 public class work0122 {
-	public int maxProfit(int[] prices) {
-		if (prices.length<=0) return 0;
+    public int maxProfit(int[] prices) {
+        if (prices.length <= 0) return 0;
         int res = 0;
         int buy = prices[0];
         int sale = prices[0];
-        for (int i = 0; i < prices.length-1; i++) {
-			while (i<prices.length-1&&prices[i]>=prices[i+1]) {
-				i++;
-			}
-			buy = prices[i];
-			while (i<prices.length-1&&prices[i]<prices[i+1]) {
-				i++;
-			}
-			sale = prices[i];
-			res += sale - buy;
-		}
+        for (int i = 0; i < prices.length - 1; i++) {
+            while (i < prices.length - 1 && prices[i] >= prices[i + 1]) {
+                i++;
+            }
+            buy = prices[i];
+            while (i < prices.length - 1 && prices[i] < prices[i + 1]) {
+                i++;
+            }
+            sale = prices[i];
+            res += sale - buy;
+        }
         return res;
     }
-	public static void main(String[] args) {
-		work0122 w= new work0122();
-		int[] prices = new int[] {7,1,5,3,6,4};
-		System.out.println(w.maxProfit(prices));
-	}
+
+    public static void main(String[] args) {
+        work0122 w = new work0122();
+        int[] prices = new int[]{7, 1, 5, 3, 6, 4};
+        System.out.println(w.maxProfit(prices));
+    }
 }
