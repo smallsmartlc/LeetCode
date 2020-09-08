25e49031b3e4d8ad74e7bd3370ad97a17b703782
smallsmartlc <1346683197@qq.com>
2020-09-08 09:56:10 +0800

    2020-9-8--0154--寻找旋转排序数组中的最小值II

--- work0154.java
+++ work0154.java
@@ -2,23 +2,24 @@ package Leetcode.smart;
 
  
 public class work0154 {
-	public int search(int[] nums) {
-		int left = 0;
-		int right = nums.length-1;
-		int mid = 0;
-		while(left<=right) {
-			if(nums[left]<nums[right]|| left == right) return nums[left];
-			mid = (left+right)/2;
-			if(nums[left]<nums[mid]) {
-				left = mid+1;
-			}else if (nums[left] == nums[mid]) {
-	            left++;
-	        }else {
-				right = mid;
+		public int search(int[] nums) {
+			int left = 0;
+			int right = nums.length-1;
+			int mid = 0;
+			while(left<=right) {
+				if(nums[left]<nums[right]|| left == right) return nums[left];
+				mid = (left+right)/2;// 二分法找中点
+				if(nums[left]<nums[mid]) {
+					left = mid+1;
+				}else if (nums[left] == nums[mid]) {
+					//无法判断数组的情况,直接顺序查找
+					left++;
+				}else {
+					right = mid;
+				}
 			}
+			return -1;
 		}
-		return -1;
-	}
 	public static void main(String[] args) {
 		work0154 w = new work0154();
 		int[] nums = new int[]{3,1,3};
