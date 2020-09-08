2749516eafb8f0a31d908925dcc84317982d6e8a
smallsmartlc <1346683197@qq.com>
2020-09-08 09:29:08 +0800

    2020-9-8--0151--翻转字符串里的单词

--- work0151.java
+++ work0151.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-public class work0151 {
+public class 	work0151 {
 	public static void main(String[] args) {
 		 work0151 w = new work0151();
 		 String s1 ="the sky is blue";
