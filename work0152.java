37b5ceb99fe84920147b6bfaaac7c4f3f42d0f4e
smallsmartlc <1346683197@qq.com>
2020-09-08 09:41:00 +0800

    2020-9-8--0152--乘积最大子数组

--- work0152.java
+++ work0152.java
@@ -14,10 +14,10 @@ public class work0152 {
 		int res = Integer.MIN_VALUE;
 		int imax = 1 , imin = 1;
 		for(int i=0; i<nums.length; i++){
-            if(nums[i] < 0){ 
-              int tmp = imax;
-              imax = imin;
-              imin = tmp;
+            if (nums[i] < 0) {
+                int tmp = imax;
+                imax = imin;
+                imin = tmp;
             }
             imax = Math.max(imax*nums[i], nums[i]);
             imin = Math.min(imin*nums[i], nums[i]);
