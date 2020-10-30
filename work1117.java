499126a1380ae6e0e06c4ac7a152b27521b6ad16
smallsmartlc <1346683197@qq.com>
2020-10-30 11:14:11 +0800

    2020-10-30--1117--H2O生成

---
+++ work1117.java
@@ -0,0 +1,45 @@
+package Leetcode.smart;
+
+import java.util.concurrent.Semaphore;
+
+/**
+ * @author LuCong
+ * @since 2020-10-30
+ **/
+public class work1117 {
+
+
+
+}
+
+
+class H2O {
+
+    private Semaphore s1,s2,s3,s4;
+
+    public H2O() {
+        s1 = new Semaphore(2);//H线程信号量
+        s2 = new Semaphore(1);//O线程信号量
+
+        s3 = new Semaphore(0);//反应条件信号量
+        s4 = new Semaphore(0);//反应条件信号量
+    }
+
+    public void hydrogen(Runnable releaseHydrogen) throws InterruptedException {
+        s1.acquire();
+        s3.release();
+        s4.acquire();
+        // releaseHydrogen.run() outputs "H". Do not change or remove this line.
+        releaseHydrogen.run();
+        s1.release();
+    }
+
+    public void oxygen(Runnable releaseOxygen) throws InterruptedException {
+        s2.acquire();
+        s4.release(2);
+        s3.acquire(2);
+        // releaseOxygen.run() outputs "O". Do not change or remove this line.
+        releaseOxygen.run();
+        s2.release();
+    }
+}
