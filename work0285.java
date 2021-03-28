0e272673bf8b345bcbbedda76e069615845253be
smallsmartlc <1346683197@qq.com>
2021-03-28 09:05:20 +0800

    2021-3-25--0285--二叉搜索树中的中序后继

--- work0285.java
+++ work0285.java
@@ -19,11 +19,11 @@ public class work0285 {
     public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
         // write your code here
         TreeNode res = null;
-        while (root != null){
-            if(p.val < root.val){
+        while (root != null) {
+            if (p.val < root.val) {
                 res = root;
                 root = root.left;
-            }else {
+            } else {
                 root = root.right;
             }
         }
