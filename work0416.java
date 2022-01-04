32ffe48319df10dcda37e72776cd0754e146ac9d
smallsmartlc <1346683197@qq.com>
2022-01-04 17:03:22 +0800

    2021-6-14--0416--分割等和子集

--- work0416.java
+++ work0416.java
@@ -34,7 +34,7 @@ public class work0416 {
         for (int i = 1; i < n; i++) {
             int num = nums[i];
             for (int j = 0; j <= target; j++) {
-                if(j >= num){
+                if(j >= num){ // 防止数组越界
                     dp[i][j] = dp[i-1][j] || dp[i-1][j-num];
                 }else{
                     dp[i][j] = dp[i-1][j];
@@ -44,5 +44,28 @@ public class work0416 {
 
         return dp[n-1][target];
     }
-
+    public boolean canPartition2(int... nums) {
+        int n = nums.length;
+        if (n < 2) {
+            return false;
+        }
+        int sum = Arrays.stream(nums).sum();
+        int max = Arrays.stream(nums).max().getAsInt();
+        int target = sum /2;
+        if(max > target) return false;
+        if (sum % 2 != 0) {
+            return false;
+        }
+        // 表示从数组内选取的数是否能使其和为j,然后不断更新
+        boolean[] dp = new boolean[target+1];
+        dp[0] = true;
+        dp[nums[0]] = true;
+        for (int i = 1; i < n; i++) {
+            int num = nums[i];
+            for (int j = target; j > num; j--) {
+                dp[j] = dp[j] || dp[j-num];
+            }
+        }
+        return dp[target];
+    }
 }
