8b81a6b786617419fb434bc98fe4db8ee807a0a7
smallsmartlc <1346683197@qq.com>
2022-01-09 15:31:11 +0800

    2020-10-27--0215--数组中的第k大元素

--- work0215.java
+++ work0215.java
@@ -1,22 +1,35 @@
 package Leetcode.smart;
 
+/**
+ * LeetCode0215 数组中的第K个最大元素
+ */
 
 import java.util.*;
 
 public class work0215 {
     public static void main(String[] args) {
         work0215 w = new work0215();
-        int[] nums = new int[]{2, 1};
-        System.out.println(w.findKthLargest2(nums, 2));
+        int[] nums = new int[]{3,2,1,5,6,4};
+        System.out.println(w.findKthLargest3(nums, 2));
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public int findKthLargest0(int[] nums, int k) {
+        Arrays.sort(nums);
+        return nums[nums.length - k];
+    }
+
+    public int findKthLargest3(int[] nums, int k) {
+        // 手写堆排序
+        new HeapSort(nums,k);
+        return nums[nums.length - k];
     }
 
     public int findKthLargest2(int[] nums, int k) {
-        //todo 自己实现堆排序
         PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
         for (int num : nums) {
             priorityQueue.add(num);
-            if(priorityQueue.size()>k) priorityQueue.poll();
-
+            if (priorityQueue.size() > k) priorityQueue.poll();
         }
         return priorityQueue.peek();
     }
@@ -26,6 +39,7 @@ public class work0215 {
     }
 
     private int quickSelect(int[] nums, int l, int r, int index) {
+        // 快速排序的过程可以确定一个元素的最终位置
         int q = sort(nums, l, r);
         if (q == index) {
             return nums[q];
@@ -46,6 +60,50 @@ public class work0215 {
         }
         nums[i] = temp;
         return i;
+    }
 
+    private static class HeapSort {
+        private int[] arr;
+
+        private void swap(int i, int j) {
+            int temp = this.arr[i];
+            this.arr[i] = this.arr[j];
+            this.arr[j] = temp;
+        }
+
+        // parent(i) = floor((i - 1)/2)
+        //left(i)   = 2i + 1
+        //right(i)  = 2i + 2
+        public HeapSort(int[] arr,int k) {
+            if (arr.length < 1) return;
+            this.arr = arr;
+            // 从最后一个根节点开始将数组变成堆
+            int lastIndex = arr.length - 1;
+            int lastRoot = (lastIndex - 1) >> 1;
+            for (int i = lastRoot; i >= 0; i--) {
+                // 将数组堆化
+                maxHeapify(i, lastIndex);
+            }
+            for (int i = lastIndex; i > lastIndex-k; i--) {
+                // 将根节点（最大值）与尾部的结点交换
+                // 并检查交换后是否除了尾部的结点是否是一个最大堆
+                swap(0, i);
+                maxHeapify(0, i - 1);
+            }
+        }
+
+        private void maxHeapify(int i, int lastIndex) {
+            int left = 2 * i + 1;
+            if (left > lastIndex) return;
+            int right = left + 1;
+            if (right <= lastIndex && arr[right] > arr[left]) {
+                left = right; // 找出更大的一个子节点
+            }
+            if (arr[left] > arr[i]) {
+                // 子节点大于父节点，交换
+                swap(left, i);
+                maxHeapify(left, lastIndex); // 交换后的子节点检查是否符合最大堆
+            }
+        }
     }
 }
