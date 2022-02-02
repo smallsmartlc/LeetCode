8bc116bde4b9fc6bed2fecd918296725a34a670c
smallsmartlc <1346683197@qq.com>
2022-02-03 00:05:49 +0800

    2022-2-3--0367--有效的完全平方数

---
+++ work0367.java
@@ -0,0 +1,28 @@
+package Leetcode.smart;
+
+//367 有效的完全平方数
+
+public class work0367 {
+
+    public static void main(String[] args) throws AlreadyTakenException, GameEndException {
+        work0367 w = new work0367();
+        System.out.println(w.isPerfectSquare(2147395600));
+    }
+
+    public boolean isPerfectSquare(int num) {
+        int l = 1,r = num;
+        int mid;
+        while (l <= r){
+            mid = l + r >> 1;
+            long n = (long) mid * mid;
+            if(n == num) return true;
+            else if(n < num && n > 0){
+                l = mid + 1;
+            }else{
+                r = mid - 1;
+            }
+        }
+        return false;
+    }
+
+}
