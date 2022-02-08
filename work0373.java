5c19efb0772a029a9beb6cada643dfecbf17e86c
smallsmartlc <1346683197@qq.com>
2022-02-08 14:49:59 +0800

    2022-2-7--0373--查找和最小的 K 对数字

--- work0373.java
+++ work0373.java
@@ -21,14 +21,14 @@ public class work0373 {
         List<List<Integer>> res = new ArrayList<>();
         PriorityQueue<List<Integer>> queue = new PriorityQueue<>((o1, o2) -> o1.get(0) + o1.get(1) - o2.get(0) - o2.get(1));
         for (int i = 0; i < Math.min(n, k); i++) {
-            for (int j = 0; j < Math.min(m, k); j++) {
+            for (int j = 0; j < Math.min(m, k - i); j++) {
                 ArrayList<Integer> list = new ArrayList<>();
                 list.add(nums1[i]);
                 list.add(nums2[j]);
                 queue.add(list);
             }
         }
-        while (!queue.isEmpty() && res.size() < k){
+        while (!queue.isEmpty() && res.size() < k) {
             res.add(queue.poll());
         }
         return res;
@@ -42,16 +42,16 @@ public class work0373 {
         PriorityQueue<int[]> queue = new PriorityQueue<>((o1, o2) -> nums1[o1[0]] + nums2[o1[1]] - nums1[o2[0]] - nums2[o2[1]]);
         // 优先队列里存储的是下标
         for (int i = 0; i < Math.min(n, k); i++) { // 先将一列坐标加入,然后从这些坐标向右找
-            queue.offer(new int[]{i,0}); // {i,j} 肯定小于{i,0},所以我们在{i,0}被取到后将{i,1}加入堆即可
+            queue.offer(new int[]{i, 0}); // {i,j} 肯定小于{i,0},所以我们在{i,0}被取到后将{i,1}加入堆即可
         }
-        while (!queue.isEmpty() && res.size() < k){
+        while (!queue.isEmpty() && res.size() < k) {
             int[] poll = queue.poll();
             ArrayList<Integer> list = new ArrayList<>();
             list.add(nums1[poll[0]]);
             list.add(nums2[poll[1]]);
             res.add(list);
-            if(poll[1] + 1 < m){
-                queue.offer(new int[]{poll[0],poll[1]+1});
+            if (poll[1] + 1 < m) {
+                queue.offer(new int[]{poll[0], poll[1] + 1});
             }
         }
         return res;
---
+++ work1001.java
@@ -0,0 +1,21 @@
+package Leetcode.smart;
+
+/**
+ * LeetCode1001. 网格照明
+ *
+ * @author LuCong
+ * @since 2020-10-30
+ **/
+public class work1001 {
+    public static void main(String[] args) {
+        work1001 w = new work1001();
+        System.out.println(w.maxSatisfied(new int[]{1, 0, 1, 2, 1, 1, 7, 5},
+                new int[]{0, 1, 0, 1, 0, 1, 0, 1}, 3));
+    }
+
+    public int[] gridIllumination(int n, int[][] lamps, int[][] queries) {
+
+    }
+}
+
+
