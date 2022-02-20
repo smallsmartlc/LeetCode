2ea0106319e60fe3c2c72b0e7b3dda7a1c848178
smallsmartlc <1346683197@qq.com>
2022-02-21 01:20:09 +0800

    2022-2-20--0396--旋转函数

--- work0396.java
+++ work0396.java
@@ -7,18 +7,17 @@ public class work0396 {
     public static void main(String[] args) {
         work0396 w = new work0396();
         System.out.println(w.maxRotateFunction(new int[]{4, 3, 2, 6}));
-
     }
 
     public int maxRotateFunction(int[] nums) {
         int n = nums.length;
-        int res = 0;
+        int total = 0;
         int sum = 0;
         for (int i = 0; i < n; i++) {
             sum += nums[i];
-            res += i * nums[i];
+            total += i * nums[i];
         }
-        int total = res;
+        int res = total;
         for (int i = 1; i < n; i++) {
             total += sum - n * nums[n - i];
             if (total > res) res = total;
