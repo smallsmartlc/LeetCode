754b155cdbf1ce36650da838ea676b7e92e660d0
smallsmartlc <1346683197@qq.com>
2022-03-03 20:42:33 +0800

    2022-3-3--1137--第 N 个泰波那契数

---
+++ work1137.java
@@ -0,0 +1,24 @@
+package Leetcode.smart;
+
+/**
+ * LeetCode0509 斐波那契数
+ */
+
+public class work1137 {
+    public static void main(String[] args) {
+        work1137 w = new work1137();
+        System.out.println(w.tribonacci(25));
+    }
+
+    public int tribonacci(int n) {
+        if (n < 2) return n;
+        if (n == 2) return 1;
+        int i = 0, j = 1, k = 1; // 滚动数组
+        while (n-- >= 2) {
+            k = i + j + k;
+            j = k - i - j;
+            i = k - j - i;
+        }
+        return j;
+    }
+}
