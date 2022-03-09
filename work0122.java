7eeee7e4d2021e3d6dec46292b1db331acaa57a4
smallsmartlc <1346683197@qq.com>
2022-03-09 19:25:32 +0800

    2020-9-7--0122--买卖股票的最佳时期 II

--- work0122.java
+++ work0122.java
@@ -4,8 +4,8 @@ public class work0122 {
     public int maxProfit(int[] prices) {
         if (prices.length <= 0) return 0;
         int res = 0;
-        int buy = prices[0];
-        int sale = prices[0];
+        int buy;
+        int sale;
         for (int i = 0; i < prices.length - 1; i++) {
             while (i < prices.length - 1 && prices[i] >= prices[i + 1]) {
                 i++;
@@ -18,6 +18,28 @@ public class work0122 {
             res += sale - buy;
         }
         return res;
+    }
+
+    public int maxProfit1(int[] prices) { // 动态规划
+        int dp1 = 0;// 不持有股票
+        int dp2 = -prices[0];// 持有股票
+        for (int i = 1; i < prices.length; i++) {
+            int dp = dp1;
+            dp1 = Math.max(dp, dp2 + prices[i]);
+            dp2 = Math.max(dp2, dp1 - prices[i]);
+        }
+        return dp1;
+    }
+
+    public int maxProfit2(int[] prices) { //贪心,收集所有上坡
+        int ans = 0;
+        int n = prices.length;
+        for (int i = 1; i < n; ++i) {
+            if (prices[i] - prices[i - 1] > 0) {
+                ans += prices[i] - prices[i - 1];
+            }
+        }
+        return ans;
     }
 
     public static void main(String[] args) {
